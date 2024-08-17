8b0d4a1a6cdeab2d17092a2ca875daddc5120a78
Saifuddin <shaikh.saifuddin@xebia.com>
2024-08-17 17:00:26 +0530

    prisma added and neon db connected

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { DatabaseModule } from './database/database.module';
+import { EmployeesModule } from './employees/employees.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, DatabaseModule, EmployeesModule],
   controllers: [AppController],
   providers: [AppService],
 })
