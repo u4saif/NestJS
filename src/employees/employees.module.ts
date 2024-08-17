5f494ef2e80a329a51cb12a402b9b52937d554fb
Saifuddin <shaikh.saifuddin@xebia.com>
2024-08-18 00:17:46 +0530

    Db added CRUD done

--- src/employees/employees.module.ts
+++ src/employees/employees.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
 import { EmployeesController } from './employees.controller';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports:[DatabaseModule],
   controllers: [EmployeesController],
   providers: [EmployeesService],
 })
